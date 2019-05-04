9c8e9f55704dc7e483e20affa61258321610e59b
William Bell <40838685+TWilliamBell@users.noreply.github.com>
2019-05-04 13:01:48 -0400

    Fixing a mistake in my derivation

--- R/c_one.d.heat.eq.R
+++ R/c_one.d.heat.eq.R
@@ -4,8 +4,8 @@ c_one.d.heat.eq <- function(n = 1000, init.dat = dgamma(seq(0, 5, 0.01),
                             save.intermediates = F) {
   boundary <- stringr::str_to_lower(boundary)
   if (isTRUE(save.intermediates)) {
-    c_heat(n, init = init.dat, boundary, alpha, dt, eps)
+    c_heat(n, init = init.dat, boundary, alpha, dt, eps, dx = dx)
   } else {
-    c_heat_memory(n, init = init.dat, boundary, alpha, dt, eps)
+    c_heat_memory(n, init = init.dat, boundary, alpha, dt, eps, dx = dx)
   }
 }
