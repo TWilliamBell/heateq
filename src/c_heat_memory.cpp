25c7bbeaa9ac188e6ff6dd7692b7213a0b478bce
William Bell <40838685+TWilliamBell@users.noreply.github.com>
2019-05-08 03:01:29 -0400

    Improving efficiency by avoiding a call to Rcpp::clone

--- src/c_heat_memory.cpp
+++ src/c_heat_memory.cpp
@@ -9,7 +9,18 @@ Rcpp::NumericVector c_heat_dirichlet_memory(int n, Rcpp::NumericVector init,
   int i;
   int j;
   int k;
-  Rcpp::NumericVector heat(Rcpp::clone(init));
+  Rcpp::NumericVector heat (space);
+  du[0] = 2.0*init[0]-init[1];
+  for (j = 1; j < space-1; j = j+1) {
+    du[j] = 2.0*init[j]-init[j-1]-init[j+1];
+  }
+  du[space-1] = 2.0*init[space-1]-init[space-2];
+  for (k = 0; k <= space-1; k = k+1) {
+    heat[k] = init[k] - lambda*du[k];
+    if (heat[k] < eps) {
+        heat[k] = 0.0;
+    }
+  }
   for (i = 1; i < n; i = i+1) {
     du[0] = 2.0*heat[0]-heat[1];
     for (j = 1; j < space-1; j = j+1) {
@@ -35,7 +46,18 @@ Rcpp::NumericVector c_heat_neumann_memory(int n, Rcpp::NumericVector init,
   int i;
   int j;
   int k;
-  Rcpp::NumericVector heat(Rcpp::clone(init));
+  Rcpp::NumericVector heat (space);
+  du[0] = init[0]-init[1];
+  for (j = 1; j < space-1; j = j+1) {
+    du[j] = 2.0*init[j]-init[j-1]-init[j+1];
+  }
+  du[space-1] = init[space-1]-init[space-2];
+  for (k = 0; k <= space-1; k = k+1) {
+    heat[k] = init[k] - lambda*du[k];
+    if (heat[k] < eps) {
+        heat[k] = 0.0;
+    }
+  }
   for (i = 1; i < n; i = i+1) {
     du[0] = heat[0]-heat[1];
     for (j = 1; j < space-1; j = j+1) {
