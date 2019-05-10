688b4c071eb47b28376cc4a57531317f5655eb36
William Bell <40838685+TWilliamBell@users.noreply.github.com>
2019-05-10 14:33:56 -0400

    Improving the flexibility of plot.heat

--- R/plot.heat.R
+++ R/plot.heat.R
@@ -7,22 +7,22 @@ plot.heat <- function(x, cex = 0.01, xaxt = "n",
     par(mfrow = c(2, 2), mar = mar)
     range.val <- range(x$full[1 , ])
     range.val[1] <- min(range.val[1], -0.01)
-    plot(x$full[1 , ], cex = 0.01,
-         yaxt = "n", xaxt = "n", ylim = range.val, ...)
-    plot(x$full[n %/% 4 , ], cex = 0.01,
-         yaxt = "n", xaxt = "n", ylim = range.val, ...)
-    plot(x$full[n %/% 2 , ], cex = 0.01,
-         yaxt = "n", xaxt = "n", ylim = range.val, ...)
-    plot(x$full[n , ], cex = 0.01,
-         yaxt = "n", xaxt = "n", ylim = range.val, ...)
+    plot(x$full[1 , ], cex = cex,
+         yaxt = yaxt, xaxt = xaxt, ylim = range.val, ...)
+    plot(x$full[n %/% 4 , ], cex = cex,
+         yaxt = yaxt, xaxt = xaxt, ylim = range.val, ...)
+    plot(x$full[n %/% 2 , ], cex = cex,
+         yaxt = yaxt, xaxt = xaxt, ylim = range.val, ...)
+    plot(x$full[n , ], cex = cex,
+         yaxt = yaxt, xaxt = xaxt, ylim = range.val, ...)
     par(mfrow = c(1, 1), mar = c(5, 4, 4, 2) + 0.1)
   } else {
     par(mfrow = c(1, 2), mar = mar)
     range.val <- range(c(x$initial.data, x$finalresults))
     range.val[1] <- min(range.val[1], -0.01)
-    plot(x$initial.data, cex = 0.01, yaxt = "n", xaxt = "n", 
+    plot(x$initial.data, cex = cex, yaxt = yaxt, xaxt = xaxt, 
          ylim = range.val, ...)
-    plot(x$finalresults, cex = 0.01, yaxt = "n", xaxt = "n", 
+    plot(x$finalresults, cex = cex, yaxt = yaxt, xaxt = xaxt, 
          ylim = range.val, ...)
     par(mfrow = c(1, 1), mar = c(5, 4, 4, 2) + 0.1)
   }
