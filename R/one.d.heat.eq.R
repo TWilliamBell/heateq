28fd642a7f6d704bdc9071a34d412c44568592f9
William Bell <40838685+TWilliamBell@users.noreply.github.com>
2019-05-10 13:27:20 -0400

    Adding parameter docs for main function

--- R/one.d.heat.eq.R
+++ R/one.d.heat.eq.R
@@ -1,10 +1,22 @@
 #' The Heat Equation Solver
-#' This function takes in all the options possible in order to choose how to solve the heat equation, the boundary conditions, and what language to use.
+#' This function takes in all the options possible in order to choose how to solve the heat equation, and the boundary conditions.
+#' @param init.dat A vector representing the evenly spaced points describing the initial state of the system.
+#' @param n The number of time steps to run the simulation for.
+#' @param alpha The rate constant for the heat equation, 'a' in du/dt = a d^2u/(dx^2).
+#' @param boundary A string, either "Neumann" or "Dirichlet" (any capitalization allowed).
+#' @param dt The length of each time step.
+#' @param eps If abs(eps) > 0, then if the value of the data at some point is less than eps, it will be truncated to zero.
+#' @param save.intermediates Recording the data at each time step is quite memory-intensive, but if one is interested in the evolution
+#' of the system rather than just the outcome, it may be desirable to record them.  For large values MALLOC in C++ might complain and
+#' shut down RStudio, in order to address this I hope to develop a rule of thumb for an average operating system (= my operating system)
+#' for how large of an array should generate an error.
+#' 
+#' @return 
 #' @export
 
 one.d.heat.eq <- function(init.dat, n = 50000,
                           boundary = c("Neumann", "Dirichlet"),
-                          alpha = 1, dt = 0.1, dx = 0.1, eps = 1e-5,
+                          alpha = 1, dt = 0.1, dx = 0.1, eps = 0,
                           save.intermediates = F) {
   if (!is.scalar(alpha) | !is.scalar(dt) | !is.scalar(eps) | !is.scalar(n)) {
     stop("Some input that should be a scalar is not.")
