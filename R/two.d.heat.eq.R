b076c18df742bb985d806091a4862f67261d06cd
William Bell <40838685+TWilliamBell@users.noreply.github.com>
2019-08-01 23:14:04 -0400

    Standardizing naming and notation

--- R/two.d.heat.eq.R
+++ R/two.d.heat.eq.R
@@ -27,7 +27,7 @@ two.d.heat.eq <- function(n = 1000, init.dat, c = 1, dt = 0.01, dx = 0.1,
   }
   results <- list(initial.data = init.dat,
                   n.steps = n, time.passed = n*dt, boundary = boundary, dim = 2L)
-  results$final <- final
+  results$final.results <- final
   class(results) <- "heat"
   results
 }
