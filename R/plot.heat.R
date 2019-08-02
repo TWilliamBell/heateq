7e4afdda69ab6de6c029e9ae09631ae93ce8c8c7
William Bell <40838685+TWilliamBell@users.noreply.github.com>
2019-08-01 23:10:56 -0400

    Consolidating OOP by consolidating plot methods

--- R/plot.heat.R
+++ R/plot.heat.R
@@ -2,28 +2,40 @@
 
 plot.heat <- function(x, cex = 0.01, xaxt = "n", 
                       yaxt = "n", mar = c(0, 0, 0, 0), ...) {
-  if (isTRUE(x$intermediates.saved)) {
-    n <- ncol(x$full)
-    par(mfrow = c(2, 2), mar = mar)
-    range.val <- range(x$full[1 , ])
-    range.val[1] <- min(range.val[1], -0.01)
-    plot(x$full[1 , ], cex = 0.01,
-         yaxt = "n", xaxt = "n", ylim = range.val, ...)
-    plot(x$full[n %/% 4 , ], cex = 0.01,
-         yaxt = "n", xaxt = "n", ylim = range.val, ...)
-    plot(x$full[n %/% 2 , ], cex = 0.01,
-         yaxt = "n", xaxt = "n", ylim = range.val, ...)
-    plot(x$full[n , ], cex = 0.01,
-         yaxt = "n", xaxt = "n", ylim = range.val, ...)
+  if (dim == 1L) {
+    if (isTRUE(x$intermediates.saved)) {
+      n <- ncol(x$full)
+      par(mfrow = c(2, 2), mar = mar)
+      range.val <- range(x$full[1 , ])
+      range.val[1] <- min(range.val[1], -0.01)
+      plot(x$full[1 , ], cex = 0.01,
+           yaxt = "n", xaxt = "n", ylim = range.val, ...)
+      plot(x$full[n %/% 4 , ], cex = 0.01,
+           yaxt = "n", xaxt = "n", ylim = range.val, ...)
+      plot(x$full[n %/% 2 , ], cex = 0.01,
+           yaxt = "n", xaxt = "n", ylim = range.val, ...)
+      plot(x$full[n , ], cex = 0.01,
+           yaxt = "n", xaxt = "n", ylim = range.val, ...)
+      par(mfrow = c(1, 1), mar = c(5, 4, 4, 2) + 0.1)
+    } else {
+      par(mfrow = c(1, 2), mar = mar)
+      range.val <- range(c(x$initial.data, x$finalresults))
+      range.val[1] <- min(range.val[1], -0.01)
+      plot(x$initial.data, cex = 0.01, yaxt = "n", xaxt = "n", 
+           ylim = range.val, ...)
+      plot(x$finalresults, cex = 0.01, yaxt = "n", xaxt = "n", 
+           ylim = range.val, ...)
+      par(mfrow = c(1, 1), mar = c(5, 4, 4, 2) + 0.1)
+    }
+  } else if (dim == 2L) {
+    plot.heat2 <- function(heat, ...) {
+    par(mfrow = c(1, 2), mar = c(5, 2, 4, 0) + 0.1)
+    range.val <- seq(from = if (heat$boundary == "neumann") {range(heat$initial.data)[1]} else if (heat$boundary == "dirichlet") {min(range(heat$initial.data)[1], 0)},
+                   to = if (heat$boundary == "neumann") {range(heat$initial.data)[2]} else if (heat$boundary == "dirichlet") {max(range(heat$initial.data)[2], 0)},
+                   length.out = 11)
+    cols <- heat.colors(10)
+    image(heat$initial.data, breaks = range.val, col = cols, ...)
+    image(heat$final, breaks = range.val, col = cols, yaxt = "n", ...)
     par(mfrow = c(1, 1), mar = c(5, 4, 4, 2) + 0.1)
-  } else {
-    par(mfrow = c(1, 2), mar = mar)
-    range.val <- range(c(x$initial.data, x$finalresults))
-    range.val[1] <- min(range.val[1], -0.01)
-    plot(x$initial.data, cex = 0.01, yaxt = "n", xaxt = "n", 
-         ylim = range.val, ...)
-    plot(x$finalresults, cex = 0.01, yaxt = "n", xaxt = "n", 
-         ylim = range.val, ...)
-    par(mfrow = c(1, 1), mar = c(5, 4, 4, 2) + 0.1)
-  }
+  } else {stop("No plot method for given dimension.")}
 }
